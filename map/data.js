e2f0670b56f11e1490831768bdcb82b27108b9b7
opadam <adam.broeken@gmail.com>
2022-08-18 16:19:59 +0200

    json data

---
+++ map/data.js
@@ -0,0 +1,93 @@
+songs = [
+  {
+    id: 0,
+    titel: "Take on Me",
+    artist: "BT Band",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a1"
+  },
+  {
+    id: 1,
+    titel: "You (DU)",
+    artist: "Bouke",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a2"
+  },
+  {
+    id: 2,
+    titel: "Country Boy",
+    artist: "Alan Jackson",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a3"
+  },
+  {
+    id: 3,
+    titel: "For the Good Times",
+    artist: "Albert West",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a4"
+  },
+  {
+    id: 4,
+    titel: "Rain And Tears",
+    artist: "Aphrodites Child",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a5"
+  },
+  {
+    id: 5,
+    titel: "You Drive Me Crazy",
+    artist: "Shakin Stevens",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a6"
+  },
+  {
+    id: 6,
+    titel: "Dance For Ever More",
+    artist: "Si Cranstoun",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a7"
+  },
+  {
+    id: 7,
+    titel: "Feel Free",
+    artist: "Bellamy Brothers",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a8"
+  },
+  {
+    id: 8,
+    titel: "Come A Little Bit Closer",
+    artist: "Jay & the Americans",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a9"
+  },
+  {
+    id: 9,
+    titel: "Candida",
+    artist: "Tony Orlando",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/a10"
+  },
+  {
+    id: 10,
+    titel: "Chiquita",
+    artist: "ABBA",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/v1"
+  },
+  {
+    id: 11,
+    titel: "Dancing Qween",
+    artist: "ABBA",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/v2"
+  },
+  {
+    id: 12,
+    titel: "Gimme! Gimme! Gimme!",
+    artist: "ABBA",
+    hoesje: "",
+    path: "https://opadam.github.io/map/map/v3"
+  },
+]
