ba924dd2c4297e22a4f67f71359a6d5a914e947b
opadam <adam.broeken@gmail.com>
2022-08-22 22:33:53 +0200

    data

--- info/data.js
+++ info/data.js
@@ -167,3 +167,100 @@ let track_list = [
     path: "https://opadam.github.io/map/map/a10"
   },
 ];
+
+let data 
+[
+  {
+    "id": 0,
+    "titel": "Take on Me",
+    "artist": "A-ha",
+    "hoesje": "https://opadam.github.io/map/img/Take_on_Me.jpg",
+    "path": "https://opadam.github.io/map/map/a1"
+  },
+  {
+    "id": 1,
+    "titel": "You (DU)",
+    "artist": "Bouke",
+    "hoesje": "https://opadam.github.io/map/img/Bouke - You_(DU).jpg",
+    "path": "https://opadam.github.io/map/map/a2"
+  },
+  {
+    "id": 2,
+    "titel": "Country Boy",
+    "artist": "Alan Jackson",
+    "hoesje": "https://opadam.github.io/map/img/Country_Boy.jpg",
+    "path": "https://opadam.github.io/map/map/a3"
+  },
+  {
+    "id": 3,
+    "titel": "For the Good Times",
+    "artist": "Albert West",
+    "hoesje": "https://opadam.github.io/map/img/For_the_Good_Times.jpg",
+    "path": "https://opadam.github.io/map/map/a4"
+  },
+  {
+    "id": 4,
+    "titel": "Rain And Tears",
+    "artist": "Aphrodites Child",
+    "hoesje": "https://opadam.github.io/map/img/Rain_And_Tears.jpg",
+    "path": "https://opadam.github.io/map/map/a5"
+  },
+  {
+    "id": 5,
+    "titel": "You Drive Me Crazy",
+    "artist": "Shakin Stevens",
+    "hoesje": "https://opadam.github.io/map/img/Shakin_Stevens.jpg",
+    "path": "https://opadam.github.io/map/map/a6"
+  },
+  {
+    "id": 6,
+    "titel": "Dance For Ever More",
+    "artist": "Si Cranstoun",
+    "hoesje": "https://opadam.github.io/map/img/Si_Cranstoun.jpg",
+    "path": "https://opadam.github.io/map/map/a7"
+  },
+  {
+    "id": 7,
+    "titel": "Feel Free",
+    "artist": "Bellamy Brothers",
+    "hoesje": "https://opadam.github.io/map/img/Feel_Free.jpg",
+    "path": "https://opadam.github.io/map/map/a8"
+  },
+  {
+    "id": 8,
+    "titel": "Come A Little Bit Closer",
+    "artist": "Jay & the Americans",
+    "hoesje": "https://opadam.github.io/map/img/Come_a_Little_Bit_Closer_Single.jpg",
+    "path": "https://opadam.github.io/map/map/a9"
+  },
+  {
+    "id": 9,
+    "titel": "Candida",
+    "artist": "Tony Orlando",
+    "hoesje": "https://opadam.github.io/map/img/Candida.jpg",
+    "path": "https://opadam.github.io/map/map/a10"
+  },
+  {
+    "id": 10,
+    "titel": "Chiquita",
+    "artist": "ABBA",
+    "hoesje": "https://opadam.github.io/map/img/Chiquita.jpg",
+    "path": "https://opadam.github.io/map/map/v1"
+  },
+  {
+    "id": 11,
+    "titel": "Dancing Qween",
+    "artist": "ABBA",
+    "hoesje": "https://opadam.github.io/map/img/Dancing_Qween.jpg",
+    "path": "https://opadam.github.io/map/map/v2"
+  },
+  {
+    "id": 12,
+    "titel": "Gimme! Gimme! Gimme!",
+    "artist": "ABBA",
+    "hoesje": "Gihttps://opadam.github.io/map/img/mme!_Gimme!_Gimme!.jpg",
+    "path": "https://opadam.github.io/map/map/v3"
+  }
+]
+
+console.log(data);
